1577d38bc65031db2334a890c8b2dbaa49a8655b
Sebastian Lehmann <sebastian@leemes.de>
2013-04-07 19:54:46 +0200

    Simplified usage. No more argument required. Three models will be generated: ...-bottom.scad, ...-top.scad, ....scad (both). The filename prefix is hardcoded (compiled).

--- main.cpp
+++ main.cpp
@@ -5,61 +5,45 @@
 #include "cubieboard.h"
 
 
+void write(const Component & model, std::string fileName)
+{
+    IndentWriter writer;
+    writer << model;
+
+    std::ofstream outFile;
+    outFile.open(fileName);
+    outFile << writer;
+}
+
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <out_file>" << std::endl;
-        std::cerr << "   where <out_file> is the target filename of the OpenSCAD file." << std::endl;
-        return 1;
-    }
-
-
     // Here, you can change the board for which a case you want to construct.
     // Don't forget to change the #include statement above.
     BoardDescription board = cubieBoard();
     CaseFactory factory(board);
+    std::string name = "cubieboard"; // File name prefix
 
     // You can set some print settings which will help the factory to optimize print results.
     factory.printLevelHeight = .2;
     factory.printSafeBridgeLayerCount = 3;
 
 
-    // Some different scene setups. Umcomment to enable.
+    // Generate the models
+    Component bottom = factory.constructBottom();
+    Component top = factory.constructTop();
+
 
     // Only the bottom
-    Component scene = factory.constructBottom();
+    write(name + "-case-bottom.scad", bottom);
 
     // Only the top
-    /*
-    Component scene = factory.constructTop();
-    */
+    write(name + "-case-top.scad", top);
 
     // Both parts side by side
-    /*
     double distance = 5; // mm
-    Component scene = factory.constructBottom()
-            + factory.constructTop()
-            .translatedCopy(0, factory.outerDimensions().y + distance, 0);
-    */
-
-    // Both parts as assembled (Only for preview. Not printable!)
-    /*
-    double distance = 0.1; // mm
-    Component scene = factory.constructBottom()
-            + factory.constructTop()
-            .rotatedCopy(180.0, 0, 0)
-            .translatedCopy(0, board.size[1], factory.outerDimensions().z + distance);
-    */
-
-
-
-    // Write the results as an OpenSCAD file.
-    IndentWriter writer;
-    writer << scene;
-    std::ofstream outFile;
-    outFile.open(argv[1]);
-    outFile << writer;
+    double offset = factory.outerDimensions().y + distance;
+    write(name + "-case.scad", bottom + top.translatedCopy(0, offset, 0));
 
     return 0;
 }
